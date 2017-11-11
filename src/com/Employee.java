3862e80369b86c434b232e524f2af1de4ea7074f
Alex Nguyen-Quoc <alexnguyenq@gmail.com>
2017-11-10 18:14:40 -0600

    Added Employee class

---
+++ src/com/Employee.java
@@ -0,0 +1,13 @@
+package com;
+
+/**
+ *
+ * @author Alexandre
+ */
+public class Employee {
+    private String lName;
+    public Employee(){}
+    public String getString(){
+        return lName;
+    }
+}
